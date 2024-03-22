ddcd878c4c41e5739d58f0f25e81161e97e1409b
Ramil Sagidullin <rasagidullin@avito.ru>
2024-03-22 10:39:13 +0300

    fix: cors

--- src/api/openai.ts
+++ src/api/openai.ts
@@ -4,4 +4,7 @@ export const openai = new OpenAI({
   apiKey: import.meta.env.VITE_PAWAN_API_KEY,
   baseURL: 'https://api.pawan.krd/pai-001/v1',
   dangerouslyAllowBrowser: true,
+  defaultHeaders: {
+    'Access-Control-Allow-Origin': '*',
+  },
 });
