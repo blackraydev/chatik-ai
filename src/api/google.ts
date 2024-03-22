ec21c9a10f272344ff37ff8f3443a255fa889d39
Ramil Sagidullin <rasagidullin@avito.ru>
2024-03-22 14:20:23 +0300

    fix

--- src/api/google.ts
+++ src/api/google.ts
@@ -1,8 +1,5 @@
 import { GoogleGenerativeAI } from '@google/generative-ai';
 
-console.log(import.meta.env)
-console.log(import.meta.env.VITE_GOOGLE_API_KEY)
-
 const googleGenAI = new GoogleGenerativeAI(import.meta.env.VITE_GOOGLE_API_KEY);
 
 export const geminiPro = googleGenAI.getGenerativeModel({ model: 'gemini-pro' });
